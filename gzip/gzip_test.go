d0251fff9c463ce48120cae241313d4f4ffe6893
David O'Rourke <david.orourke@gmail.com>
2014-05-21 11:32:39 +0100

    Added tests.

---
+++ gzip/gzip_test.go
@@ -0,0 +1,92 @@
+package gzip
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const (
+	gzipTestString = "Foobar Wibble Content"
+)
+
+func testHTTPContent(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, gzipTestString)
+}
+
+func Test_ServeHTTP_Compressed(t *testing.T) {
+	gzipHandler := Gzip(DefaultCompression)
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "http://localhost/foobar", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set(headerAcceptEncoding, encodingGzip)
+
+	gzipHandler.ServeHTTP(w, req, testHTTPContent)
+
+	gr, err := gzip.NewReader(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer gr.Close()
+
+	body, _ := ioutil.ReadAll(gr)
+
+	if string(body) != gzipTestString {
+		t.Fail()
+	}
+}
+
+func Test_ServeHTTP_NoCompression(t *testing.T) {
+	gzipHandler := Gzip(NoCompression)
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "http://localhost/foobar", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gzipHandler.ServeHTTP(w, req, testHTTPContent)
+
+	if w.Body.String() != gzipTestString {
+		t.Fail()
+	}
+}
+
+func Test_ServeHTTP_CompressionWithNoGzipHeader(t *testing.T) {
+	gzipHandler := Gzip(DefaultCompression)
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "http://localhost/foobar", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gzipHandler.ServeHTTP(w, req, testHTTPContent)
+
+	if w.Body.String() != gzipTestString {
+		t.Fail()
+	}
+}
+
+func Test_ServeHTTP_InvalidCompressionLevel(t *testing.T) {
+	gzipHandler := Gzip(11)
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "http://localhost/foobar", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set(headerAcceptEncoding, encodingGzip)
+
+	gzipHandler.ServeHTTP(w, req, testHTTPContent)
+
+	if w.Body.String() != gzipTestString {
+		t.Fail()
+	}
+}
