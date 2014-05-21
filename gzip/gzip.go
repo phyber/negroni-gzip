0bc790efb68ac5a8c810d49ed3400bb2b833216f
David O'Rourke <david.orourke@gmail.com>
2014-05-21 09:59:05 +0100

    Many changes. Extra documentation.

--- gzip/gzip.go
+++ gzip/gzip.go
@@ -1,3 +1,4 @@
+// Package gzip implements a gzip compression handler middleware for Negroni.
 package gzip
 
 import (
@@ -8,53 +9,81 @@ import (
 	"github.com/codegangsta/negroni"
 )
 
+// These compression constants are copied from the compress/gzip package.
 const (
-	HeaderAcceptEncoding  = "Accept-Encoding"
-	HeaderContentEncoding = "Content-Encoding"
-	HeaderContentLength   = "Content-Length"
-	HeaderContentType     = "Content-Type"
-	HeaderVary            = "Vary"
+	encodingGzip = "gzip"
+
+	headerAcceptEncoding  = "Accept-Encoding"
+	headerContentEncoding = "Content-Encoding"
+	headerContentLength   = "Content-Length"
+	headerContentType     = "Content-Type"
+	headerVary            = "Vary"
+
+	BestCompression    = gzip.BestCompression
+	BestSpeed          = gzip.BestSpeed
+	DefaultCompression = gzip.DefaultCompression
+	NoCompression      = gzip.NoCompression
 )
 
+// gzipResponseWriter is the ResponseWriter that negroni.ResponseWriter is
+// wrapped in.
 type gzipResponseWriter struct {
 	w *gzip.Writer
 	negroni.ResponseWriter
 }
 
+// Write writes bytes to the gzip.Writer. It will also set the Content-Type
+// header using the net/http library content type detection if the Content-Type
+// header was not set yet.
 func (grw gzipResponseWriter) Write(b []byte) (int, error) {
-	if len(grw.Header().Get(HeaderContentType)) == 0 {
-		grw.Header().Set(HeaderContentType, http.DetectContentType(b))
+	if len(grw.Header().Get(headerContentType)) == 0 {
+		grw.Header().Set(headerContentType, http.DetectContentType(b))
 	}
 	return grw.w.Write(b)
 }
 
-type Gzipper struct{}
-
-// Returns our Gzipper that adds gzip compression to all requests
-func Gzip() *Gzipper {
-	return &Gzipper{}
+// handler struct contains the ServeHTTP method and the compressionLevel to be
+// used.
+type handler struct {
+	compressionLevel int
 }
 
-func (g *Gzipper) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	if !strings.Contains(r.Header.Get(HeaderAcceptEncoding), "gzip") {
+// Gzip returns our handler which will handle the Gzip compression in ServeHTTP.
+// Valid values for level are identical to those in the compress/gzip package.
+func Gzip(level int) *handler {
+	return &handler{
+		compressionLevel: level,
+	}
+}
+
+// ServeHTTP wraps the http.ResponseWriter with a gzip.Writer.
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	// Skip compression if the client doesn't accept gzip encoding.
+	if !strings.Contains(r.Header.Get(headerAcceptEncoding), encodingGzip) {
 		next(w, r)
 		return
 	}
 
-	headers := w.Header()
-	headers.Set(HeaderContentEncoding, "gzip")
-	headers.Set(HeaderVary, HeaderAcceptEncoding)
-
-	gz := gzip.NewWriter(w)
+	// Create new gzip Writer. Skip compression if an invalid compression level
+	// was set.
+	gz, err := gzip.NewWriterLevel(w, h.compressionLevel)
+	if err != nil {
+		next(w, r)
+		return
+	}
 	defer gz.Close()
 
-	gzw := gzipResponseWriter{
+	headers := w.Header()
+	headers.Set(headerContentEncoding, encodingGzip)
+	headers.Set(headerVary, headerAcceptEncoding)
+
+	grw := gzipResponseWriter{
 		gz,
 		w.(negroni.ResponseWriter),
 	}
 
-	next(gzw, r)
+	next(grw, r)
 
-	// delete content length after we know we have been written to
-	gzw.Header().Del(HeaderContentLength)
+	// Delete the content length after we know we have been written to.
+	grw.Header().Del(headerContentLength)
 }
